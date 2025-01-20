c541e06c236588ee3eabe8ec902861b8e79c4707
thekbbohara <ytkbzcraft@gmail.com>
2025-01-20 17:25:24 +0545

    arrow pos

--- src/app/Home/sellingPoint.tsx
+++ src/app/Home/sellingPoint.tsx
@@ -11,11 +11,11 @@ const SellingPoint = () => {
     <div className='flex justify-evenly my-20'>
       <div className='flex relative'>
         <p>Felling Overwhelmed by <br /> technical interviews</p>
-        <Image src={"/arrow.svg"} alt="->" width={50} height={50} className='absolute -right-[72] -top-5 select-none' />
+        <Image src={"/arrow.svg"} alt="->" width={50} height={50} className='absolute  select-none' style={{ right: "-72px", top: "-5px" }} />
       </div>
       <div className='flex relative'>
         <p>Struggling with time pressure during <br /> coding challenges</p>
-        <Image src={"/arrow.svg"} alt="->" width={50} height={50} className='-top-5 -right-[72] absolute select-none' />
+        <Image src={"/arrow.svg"} alt="->" width={50} height={50} className='absolute select-none' style={{ right: "-72px", top: "-6px" }} />
       </div>
       <p>Losing confidence after <br /> failed interviews</p>
     </div>
