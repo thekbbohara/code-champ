773fa93bbb58b3a16b6dfe9fdf56ad70200ce5ae
thekbbohara <ytkbzcraft@gmail.com>
2025-01-21 21:41:51 +0545

    my-4

--- src/components/common/codeChamp.tsx
+++ src/components/common/codeChamp.tsx
@@ -1,7 +1,7 @@
 import { cn } from "@/lib/utils"
 import Image from "next/image"
 
-export const CodeChamp = ({ height = 50, width = 50 }) => {
-  return <Image className={cn("mix-blend-lighten")} src={"/code-champ.png"} alt="code-champ" height={height} width={width} />
+export const CodeChamp = ({ height = 50, width = 50, className = "my-0" }) => {
+  return <Image className={cn(className, "mix-blend-lighten")} src={"/code-champ.png"} alt="code-champ" height={height} width={width} />
 }
 
