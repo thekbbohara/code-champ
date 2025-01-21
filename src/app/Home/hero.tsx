06b9fae594fff2f449a9d64f297949847227581f
thekbbohara <ytkbzcraft@gmail.com>
2025-01-21 21:47:25 +0545

    my-4

--- src/app/Home/hero.tsx
+++ src/app/Home/hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <section className="min-h-[90vh] h-full flex flex-col-reverse sm:my-0 mt-6 mb-24 sm:flex-row  justify-between items-center">
       <div className="my-auto flex flex-col gap-4">
-        <h1 className="font-bold text-[46px] leading-10 sm:leading-9 text-gray-200"><strong>Master Technical Interviews Through Real Competition</strong></h1>
+        <h1 className="font-bold text-[46px] leading-10 sm:leading-9 sm:my-0 my-4 text-gray-200"><strong>Master Technical Interviews Through Real Competition</strong></h1>
         <h2 className="text-gray-400 font-medium ">Practical coding challenges in real-time battles against other developers. Win tokens, build confidence, and land your dream job.</h2>
 
         <Link href={"/auth"} className=" w-fit bg-white px-3 py-2 rounded-md font-semibold text-gray-700" >
