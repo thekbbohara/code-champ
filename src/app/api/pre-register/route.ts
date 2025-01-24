abc295e322c9cbfbe670989b7ad31332775b5820
thekbbohara <ytkbzcraft@gmail.com>
2025-01-25 04:55:49 +0545

    pre reg api

--- src/app/api/pre-register/route.ts
+++ src/app/api/pre-register/route.ts
@@ -62,7 +62,7 @@ export async function GET(request: Request) {
   try {
     const { searchParams } = new URL(request.url);
     const email = searchParams.get("email");
-
+    //console.log(email);
     if (!email) {
       return NextResponse.json({ error: "Email is required" }, { status: 400 });
     }
@@ -70,6 +70,7 @@ export async function GET(request: Request) {
     const user = await prisma.user.findUnique({
       where: { email },
     });
+    //console.log("user", user);
 
     if (!user) {
       return NextResponse.json({ error: "User not found" }, { status: 404 });
