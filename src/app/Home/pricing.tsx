829841bfea1e37edb63241ae9d543b5fa1fb5a0b
thekbbohara <ytkbzcraft@gmail.com>
2025-02-01 00:55:08 +0545

    pricking out

--- src/app/Home/pricing.tsx
+++ src/app/Home/pricing.tsx
@@ -1,41 +1,50 @@
-const Pricing = () => {
-  const pricingPlans = [
-    {
-      name: 'Starter',
-      price: 5,
-      coins: '50 coins',
-      features: [
-        'Basic performance analytics',
-        'Global chat access',
-      ],
-      color: 'bg-transparent text-white border border-gray-600',
-    },
-    {
-      name: 'Pro',
-      price: 15,
-      coins: '200 coins',
-      features: [
-        'Advanced analytics',
-        'Priority matching',
-        'Chat with other pros',
-      ],
-      color: 'bg-transparent text-white border border-gray-600',
-      popular: true,
-    },
+export interface PricingPlan {
+  name: string,
+  price: number,
+  tokens: number,
+  features: string[],
+  color: string,
+  popular?: boolean;
+}
+export const pricingPlans: PricingPlan[] = [
+  {
+    name: 'Starter',
+    price: 6,
+    tokens: 9,
+    features: [
+      'Basic performance analytics',
+      'Global chat access',
+    ],
+    color: 'bg-transparent text-white border border-gray-600',
+  },
+  {
+    name: 'Pro',
+    price: 15,
+    tokens: 30,
+    features: [
+      'Advanced analytics',
+      'Priority matching',
+      'Chat with other pros',
+    ],
+    color: 'bg-transparent text-white border border-gray-600',
+    popular: true,
+  },
 
-    {
-      name: 'Big Boss',
-      price: 25,
-      coins: '500 coins',
-      features: [
-        'Everything in Pro',
-        'Organize competitions',
-        'Create custom challenges',
-        'Room analytics',
-      ],
-      color: 'bg-transparent text-white border border-gray-600',
-    },
-  ];
+  {
+    name: 'Big Boss',
+    price: 34.5,
+    tokens: 69,
+    features: [
+      'Everything in Pro',
+      'Organize competitions',
+      'Create custom challenges',
+      'Room analytics',
+    ],
+    color: 'bg-transparent text-white border border-gray-600',
+  },
+];
+const Pricing = () => {
+
 
   return (
     <section className="flex flex-col gap-6 justify-center items-center my-20" id="pricing">
@@ -53,7 +62,7 @@ const Pricing = () => {
               <span className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-orange-500 text-white px-3 py-1 text-sm font-semibold rounded-full">Popular</span>
             )}
             <h3 className="text-2xl font-bold mb-4 text-center">{plan.name}</h3>
-            <p className="text-3xl font-bold mb-6 text-center">{plan.coins}</p>
+            <p className="text-3xl font-bold mb-6 text-center">{plan.tokens}</p>
             <ul className="space-y-4 mb-6">
               {plan.features.map((feature, i) => (
                 <li key={i} className="flex items-center gap-2 text-lg">
